6510ea4f6614bd11d3c1f53e1fe43b4db3281f10
Matt Brophy <matt@brophy.org>
2018-11-14 22:10:30 -0500

    Add vuex store size checks

--- src/store.js
+++ src/store.js
@@ -3,14 +3,32 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const STORE_WARN_THRESHOLD = 10;
+const STORE_ERROR_THRESHOLD = 20;
+
+const store = new Vuex.Store({
   state: {
-
+      foo: 'Hello World!',
+      bar: 'Hello World!',
+      baz: 'Hello World!',
   },
-  mutations: {
+  mutations: {},
+  actions: {},
+});
 
-  },
-  actions: {
+if (process.env.NODE_ENV !== 'production') {
+    const storeSize = JSON.stringify(store.state).length;
+    if (storeSize > STORE_ERROR_THRESHOLD) {
+        console.error(
+            `[ERROR] Store size (${storeSize}) exceeds error threshold`,
+            `of ${STORE_ERROR_THRESHOLD}`,
+        );
+    } else if (storeSize > STORE_WARN_THRESHOLD) {
+        console.warn(
+            `[WARN] Store size (${storeSize}) exceeds warning threshold`,
+            `of ${STORE_WARN_THRESHOLD}`,
+        );
+    }
+}
 
-  }
-})
+export default store;
